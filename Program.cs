3a35103c87585bc2717eb31f78b08391f2a69612
fifty-six <ybham6@gmail.com>
2022-07-04 12:19:33 -0400

    Task for each link when dealing with multi-os link sets

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Net.Http;
 using System.Xml;
 using System.Security.Cryptography;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
@@ -16,47 +15,49 @@ namespace ModlinksShaVerifier
     {
         private static readonly HttpClient _Client = new();
 
-        private static string ShaToString(byte[] hash) 
+        private static string ShaToString(byte[] hash)
             => BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
 
-        private static async Task<bool> CheckSingleSha(Manifest m)
+        private static async Task<bool> CheckLink(Manifest m, Link link)
         {
             using var sha = SHA256.Create();
 
-            Console.WriteLine($"Checking '{m.Name}'");
+            Stream stream;
 
-            foreach (var link in m.Links.AsEnumerable())
+            try
             {
-                Stream stream;
-
-                try
-                {
-                    stream = await _Client.GetStreamAsync(link.URL);
-                }
-                catch (HttpRequestException e)
-                {
-                    WriteError("Check", $"Request failed for {m.Name} - {link.URL}! {e.StatusCode}");
-                    return false;
-                }
-                
-                string shasum = ShaToString(await sha.ComputeHashAsync(stream));
-
-                if (shasum == link.SHA256.ToLowerInvariant()) 
-                    continue;
-                
-                WriteError("Check", $"Hash mismatch of {m.Name} in link {link.URL}. Expected: {link.SHA256}, got {shasum}");
-                
+                stream = await _Client.GetStreamAsync(link.URL);
+            }
+            catch (HttpRequestException e)
+            {
+                WriteError("Check", $"Request failed for {m.Name} - {link.URL}! {e.StatusCode}");
                 return false;
             }
-            
-            return true;
+
+            string shasum = ShaToString(await sha.ComputeHashAsync(stream));
+
+            if (shasum == link.SHA256.ToLowerInvariant())
+                return true;
+
+            WriteError("Check", $"Hash mismatch of {m.Name} in link {link.URL}. Expected: {link.SHA256}, got {shasum}");
+
+            return false;
         }
-        
+
+        private static async Task<bool> CheckSingleSha(Manifest m)
+        {
+            Console.WriteLine($"Checking '{m.Name}'");
+
+            var res = await Task.WhenAll(m.Links.AsEnumerable().Select(x => CheckLink(m, x)));
+
+            return res.All(x => x);
+        }
+
         internal static async Task<int> Main(string[] args)
         {
             var sw = new Stopwatch();
             sw.Start();
-            
+
             if (args.Length != 1)
             {
                 await Console.Error.WriteLineAsync("Usage: ModlinksShaVerifier [FILE]");
@@ -71,8 +72,8 @@ namespace ModlinksShaVerifier
                 return 1;
             }
 
-            var reader = XmlReader.Create(path, new XmlReaderSettings { Async = true });
-            
+            var reader = XmlReader.Create(path, new XmlReaderSettings {Async = true});
+
             var serializer = new XmlSerializer(typeof(Manifest));
 
             List<Task<bool>> checks = new();
@@ -81,25 +82,26 @@ namespace ModlinksShaVerifier
             {
                 if (reader.NodeType != XmlNodeType.Element)
                     continue;
-                
+
                 if (reader.Name != "Manifest")
                     continue;
-                
+
                 var manifest = (Manifest?) serializer.Deserialize(reader) ?? throw new InvalidDataException();
-                
+
                 checks.Add(CheckSingleSha(manifest));
             }
 
             var res = await Task.WhenAll(checks);
-            
+
             sw.Stop();
-            
+
             Console.WriteLine($"Completed in {sw.ElapsedMilliseconds}ms.");
 
             // If they're not all correct, error.
             return !res.All(x => x) ? 1 : 0;
         }
 
-        private static void WriteError(string title, string message) => Console.WriteLine($"::error title={title}::{message}");
+        private static void WriteError(string title, string message) =>
+            Console.WriteLine($"::error title={title}::{message}");
     }
 }
